d6bd19c9fe199e23c1ae341bab51d5b86d40e066
Nora Granahan <namgranahan@gmail.com>
2018-02-21 21:25:32 -0500

    updated alerts and positioning of dynamic elements

--- javascript/game.js
+++ javascript/game.js
@@ -1,40 +1,26 @@
 
-// 1. Game page loads with instructions for user to press any key to start (all html and css)
-// 2. When user presses any key computer selects random word from a list of possible words
-// 3. Computer determines the length of the selected word
-// 4. Function applies a number of divs, equal to the length of the word, to the html, to represent place holders for the letters. Each placeholder div needs to have an attribute tag that corresponds to the character that it represents in the string.
-// 5. User receives a notification that they have x number of guesses. X should be equal to length of the selected word times 2.
-// 6.  each time the user makes a guess, a counter is updated and added to the html to show how many guesses remain.
-// 7. within the same function, each user guess will also display to the screen.
-// 8. Computer evaluates if user guess equals any character in string
-// 9. If the user guess equals any letter in the string, that particular placeholder div should be replaced with the letter.
-// 10. If all of the letters in the string are selected before the number of guesses equals zero, a notification that the user won will display.
-// 11. If user guesses equals zero before the user selects all of the letters in the string, a notification that the user lost will display and ask the user to play again.
-// 12. keep track of wins
+// Things I would Fix with More Time: 1. had trouble figuring out how to start game with initial key stroke and collect user guesses with each following keystroke. Used start button to get around this first step. 2. Prevent guessesLeft from going into negative numbers if player continues to press buttons 3. Display alert when player loses. Seems like comparison code isn't working.
 
-// Issues: 1. had trouble figuring out how to start game with initial key stroke and collect user guesses with each following keystroke. Used start button to get around this first step. Will go back to fix if time allows. 2. Determine if player has won. 3. 
 
-//Additional Steps If Time
-// 13. If var keyChar (letter guessed) != any letter that has already been guessed append keyChar to HTML, else do nothing
-// 14. If guessesLeft === wordLength display hint alert. Hint will be alert a rhyming word.
-//15.
-
-var words = ["milkshake", "cupcake", "sunday", "donut", "popsicle", "cake"];
+var words = ["milkshake", "cupcake", "sundae", "donut", "popsicle", "cake"];
 var keyStrokesCounter = 0;
 var computerPick = "";
 var guessDisplay = "";
+var allGuesses = [];
 var numberGuesses = 0;
 var guessesLeft = 0;
 var userPick = [];
 var wins = 0;
-var indices = [];
+var loseMessage = "";
 
 // Step 2, Step 3, Step 4, Step 5
 // Start Button Function
 function startGame() {
     document.getElementById("word-divs").innerHTML = "";
     document.getElementById("letterGuessed").innerHTML = "";
+    document.getElementById("winAlert").innerHTML = "";
     userPick = [];
+    allGuesses = [];
     keyStrokesCounter = 0;
     computerPick = words[Math.floor(Math.random() * words.length)]
     console.log("Random Word: " + computerPick);
@@ -53,50 +39,51 @@ function startGame() {
     //changes image to match computer selected word
     if (computerPick === "milkshake") {
         document.getElementsByClassName("img-active")[0].setAttribute("class", "img-responsive center-block hide");
-        document.getElementById("milkshake").setAttribute("class", "img-responsive center-block img-active").classList.remove("hide");
+        document.getElementById("milkshake").setAttribute("class", "img-responsive center-block img-active");
     } else if (computerPick === "cupcake") {
         document.getElementsByClassName("img-active")[0].setAttribute("class", "img-responsive center-block hide");
-        document.getElementById("cupcake").setAttribute("class", "img-responsive center-block img-active").classList.remove("hide");
-    } else if (computerPick === "sunday") {
+        document.getElementById("cupcake").setAttribute("class", "img-responsive center-block img-active");
+    } else if (computerPick === "sundae") {
         document.getElementsByClassName("img-active")[0].setAttribute("class", "img-responsive center-block hide");
-        document.getElementById("sunday").setAttribute("class", "img-responsive center-block img-active").classList.remove("hide");
+        document.getElementById("sundae").setAttribute("class", "img-responsive center-block img-active");
     } else if (computerPick === "donut") {
         document.getElementsByClassName("img-active")[0].setAttribute("class", "img-responsive center-block hide");
-        document.getElementById("donut").setAttribute("class", "img-responsive center-block img-active").classList.remove("hide");
+        document.getElementById("donut").setAttribute("class", "img-responsive center-block img-active");
     } else if (computerPick === "popsicle") {
         document.getElementsByClassName("img-active")[0].setAttribute("class", "img-responsive center-block hide");
-        document.getElementById("popsicle").setAttribute("class", "img-responsive center-block img-active").classList.remove("hide");
+        document.getElementById("popsicle").setAttribute("class", "img-responsive center-block img-active");
     } else if (computerPick === "cake") {
         document.getElementsByClassName("img-active")[0].setAttribute("class", "img-responsive center-block hide");
-        document.getElementById("cake").setAttribute("class", "img-responsive center-block img-active").classList.remove("hide");
+        document.getElementById("cake").setAttribute("class", "img-responsive center-block img-active");
     }
 }
 
 // Step 6, Step 7, Step 8, Step 9
 
 document.onkeyup = function (event) {
-    keyStrokesCounter++
-    guessesLeft = numberGuesses - keyStrokesCounter;
-    document.getElementsByClassName("display-guesses")[0].innerHTML = guessesLeft;
     var keyChar = String.fromCharCode(event.keyCode).toLowerCase();
-    var guessed = document.createElement("div");
-    document.getElementById("letterGuessed").appendChild(guessed).setAttribute("class", "guessed-letters");
-    document.getElementsByClassName("guessed-letters")[0].append(" " + keyChar);
+    var allGuessesStr = allGuesses.toString();
+    //if key pressed does not equal any of the previously pressed keys, do the following
+    if (allGuessesStr.includes(keyChar) === false) {
+        keyStrokesCounter++
+        guessesLeft = numberGuesses - keyStrokesCounter;
+        document.getElementsByClassName("display-guesses")[0].innerHTML = guessesLeft;
+        allGuesses.push(" " + keyChar);
+        document.getElementById("letterGuessed").innerHTML = allGuesses;
+    }
+    
     if (computerPick.includes(keyChar)) {
-
         //finds each index for characters that appear multiple times and pushes them to their specific location in userPick array
         for (var n = 0; n < computerPick.length; n++) {
             if (computerPick[n] === keyChar) {
                 userPick.splice(n, 1, keyChar);
             }
         }
-
         var x = document.getElementsByClassName(keyChar);
         for (var j = 0; j < x.length; j++) {
             x[j].innerHTML = keyChar;
             x[j].classList.remove("place-holder");
         }
-
         //determines wins and losses
         if (computerPick.length !== userPick.length)
             return false;
@@ -104,17 +91,25 @@ document.onkeyup = function (event) {
             if (computerPick[a] !== userPick[a])
                 return false;
         }
-        // alert("you win");
+        loseMessage = document.createElement("div");
+        document.getElementById("winAlert").appendChild(loseMessage).setAttribute("class", "special alert alert-success alert-dismissible");
+        document.getElementsByClassName("special")[0].innerHTML = "<p>Yay! You won. Now let's play again!</p>";
         wins++
         var displayWins = document.createElement("div");
         document.getElementById("wins").appendChild(displayWins).setAttribute("class", "win-count");
         document.getElementsByClassName("win-count")[0].innerHTML = wins;
     }
-
-
-
+    //not running for some reason. needs testing.
+    if (guessesLeft === 0) {
+        alert("you lose.")
+        loseMessage = document.createElement("div");
+        document.getElementById("loseAlert").appendChild(loseMessage).setAttribute("class", "lose alert alert-warning alert-dismissible");
+        document.getElementsByClassName("lose")[0].innerHTML = "<p>No sprinkles for you! Let's play again!</p>";
+    }
 
 }
+
+
 
 
 
